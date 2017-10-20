97d0bedfff6f2d6a7ab161f8baf62e03712d85d0
Jan Lievens <jan.lievens@gmail.com>
2017-10-20 14:02:49 +0200

    register in the main thread plz

--- ios/RCTVideo.m
+++ ios/RCTVideo.m
@@ -455,21 +455,23 @@ static NSString *const timedMetadata = @"timedMetadata";
 - (void)attachListeners
 {
   // listen for end of file
-  [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                  name:AVPlayerItemDidPlayToEndTimeNotification
-                                                object:[_player currentItem]];
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(playerItemDidReachEnd:)
-                                               name:AVPlayerItemDidPlayToEndTimeNotification
-                                             object:[_player currentItem]];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:AVPlayerItemDidPlayToEndTimeNotification
+                                                  object:[_player currentItem]];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerItemDidReachEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:[_player currentItem]];
 
-  [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                  name:AVPlayerItemPlaybackStalledNotification
-                                                object:[_player currentItem]];
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(playbackStalled:)
-                                               name:AVPlayerItemPlaybackStalledNotification
-                                             object:[_player currentItem]];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:AVPlayerItemPlaybackStalledNotification
+                                                  object:[_player currentItem]];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playbackStalled:)
+                                                 name:AVPlayerItemPlaybackStalledNotification
+                                               object:[_player currentItem]];
+  });
 }
 
 - (void)playbackStalled:(NSNotification *)notification
